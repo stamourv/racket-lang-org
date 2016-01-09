4b3f6fcd61f27189b537225f0c8e4d037f338d05
Matthew Butterick <mb@mbtype.com>
2016-01-09 13:29:32 -0800

    update fonts

--- pollen/www/fonts/fonts.css.pp
+++ pollen/www/fonts/fonts.css.pp
@@ -4,5 +4,11 @@
 ◊(ffd/rp "cooper-hewitt" "CooperHewitt-Book.otf" 
 #:font-style "normal" #:font-weight "normal" #:base64 #t)
 
+◊(ffd/rp "cooper-hewitt" "CooperHewitt-Medium.otf" 
+#:font-style "normal" #:font-weight "bold" #:base64 #t)
+
 ◊(ffd/rp "source-code-pro" "SourceCodePro-Regular.otf" 
 #:font-style "normal" #:font-weight "normal" #:base64 #t)
+
+◊(ffd/rp "source-code-pro" "SourceCodePro-Medium.otf" 
+#:font-style "normal" #:font-weight "bold" #:base64 #t)
