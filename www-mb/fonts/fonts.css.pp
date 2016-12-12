b71ed24e17a6179f93b1971a55fb8c931e9a548d
Matthew Butterick <mb@mbtype.com>
2016-12-12 13:10:53 -0800

    consolidated commit

---
+++ www-mb/fonts/fonts.css.pp
@@ -0,0 +1,17 @@
+#lang pollen
+◊(require css-tools/font-face)
+
+◊(ffd/rp "cooper-hewitt" "CooperHewitt-Book.otf" 
+#:font-style "normal" #:font-weight "normal" #:base64 #t)
+
+◊(ffd/rp "cooper-hewitt" "CooperHewitt-Medium.otf" 
+#:font-style "normal" #:font-weight "bold" #:base64 #t)
+
+◊(ffd/rp "source-code-pro" "sourcecodepro-light.woff" 
+#:font-style "normal" #:font-weight "300" #:base64 #t)
+
+◊(ffd/rp "source-code-pro" "sourcecodepro-regular.woff" 
+#:font-style "normal" #:font-weight "normal" #:base64 #t)
+
+◊(ffd/rp "source-code-pro" "sourcecodepro-medium.woff" 
+#:font-style "normal" #:font-weight "bold" #:base64 #t)
---
+++ www-mb/template.html
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Racket</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="styles.css" media="screen"/>
+<link rel="stylesheet" href="fonts/fonts.css" media="screen"/>
+<link rel="stylesheet" href="fonts/feather/feather.css" media="screen"/>
+<script type="text/javascript" src="functions.js"></script>
+
+</head>
+<body>
+
+◊(->html doc)
+
+
+</body>
+
+
+
+</html>
